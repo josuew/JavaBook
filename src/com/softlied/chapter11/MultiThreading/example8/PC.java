b592809347fe1e16073cc95c0364c8196927e415
JaurePerro <budokaii_joshua@hotmail.com>
2024-04-05 18:56:10 -0600

    Threads Section

---
+++ src/com/softlied/chapter11/MultiThreading/example8/PC.java
@@ -0,0 +1,62 @@
+package com.softlied.chapter11.MultiThreading.example8;
+
+class Q{
+    int n;
+
+    synchronized int get(){
+        System.out.println("Got: " + n);
+        return n;
+    }
+
+    synchronized void put(int n){
+        this.n = n;
+        System.out.println("Put: " + n);
+    }
+}
+
+class Producer implements Runnable{
+    Q q;
+    Thread t;
+
+    public Producer(Q q){
+        this.q = q;
+        t = new Thread(this, "Producer");
+    }
+
+    public void run(){
+        int i = 0;
+
+        while (true){
+            q.put(i++);
+        }
+    }
+}
+
+class Consumer implements Runnable{
+    Q q;
+    Thread t;
+
+    public Consumer(Q q){
+        this.q = q;
+        t = new Thread(this, "Consumer");
+    }
+
+    public void run(){
+        while (true){
+            q.get();
+        }
+    }
+}
+
+public class PC {
+    public static void main(String[] args) {
+        Q q = new Q();
+        Producer p = new Producer(q);
+        Consumer c = new Consumer(q);
+
+        // Start the threads
+        p.t.start();
+        c.t.start();
+
+    }
+}
---
+++ src/com/softlied/chapter11/MultiThreading/example9/PCFixed.java
@@ -0,0 +1,78 @@
+package com.softlied.chapter11.MultiThreading.example9;
+
+class Q{
+    int n;
+    boolean valueSet = false;
+
+    synchronized int get(){
+        while (!valueSet)
+            try {
+                wait();
+            }catch (InterruptedException ex){
+                System.out.println("InterruptedException Caught");
+            }
+        System.out.println("Got: " + n);
+        valueSet = false;
+        notify();
+        return n;
+    }
+
+    synchronized void put(int n){
+        while (valueSet)
+            try {
+                wait();
+            }catch (InterruptedException ex){
+                System.out.println("InterruptedException Caught");
+            }
+        this.n = n;
+        valueSet = true;
+        System.out.println("Put: " + n);
+        notify();
+    }
+}
+
+class Producer implements Runnable{
+    Q q;
+    Thread t;
+
+    public Producer(Q q){
+        this.q = q;
+        t = new Thread(this, "Producer");
+    }
+
+    public void run(){
+        int i = 0;
+
+        while (true){
+            q.put(i++);
+        }
+    }
+}
+
+class Consumer implements Runnable{
+    Q q;
+    Thread t;
+
+    public Consumer(Q q){
+        this.q = q;
+        t = new Thread(this, "Consumer");
+    }
+
+    public void run(){
+        while (true){
+            q.get();
+        }
+    }
+}
+
+public class PCFixed {
+    public static void main(String[] args) {
+        Q q = new Q();
+        Producer p = new Producer(q);
+        Consumer c = new Consumer(q);
+
+        p.t.start();
+        c.t.start();
+
+    }
+}
