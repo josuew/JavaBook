f665b196dfdde2e2b7d78df743d380893c9c4144
JaurePerro <budokaii_joshua@hotmail.com>
2024-05-05 17:22:37 -0600

    Strings

---
+++ src/com/softlied/chapter17/StringHandling/example9/charAtDemo.java
@@ -0,0 +1,9 @@
+package com.softlied.chapter17.StringHandling.example9;
+
+public class charAtDemo {
+    public static void main(String[] args) {
+        char ch;
+        ch = "abc".charAt(1);
+        System.out.println(ch);
+    }
+}
