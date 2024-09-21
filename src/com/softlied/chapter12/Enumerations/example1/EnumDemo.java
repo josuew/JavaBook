1abf3e54bc4cea7d74292766c701470f2d17419c
JaurePerro <budokai_joshua@hotmail.com>
2024-09-20 23:03:59 -0600

    Annotations

--- src/com/softlied/chapter12/Enumerations/example1/EnumDemo.java
+++ src/com/softlied/chapter12/Enumerations/example1/EnumDemo.java
@@ -24,19 +24,18 @@ public class EnumDemo {
             case Jonathan:
                 System.out.println("Jonathan is red");
                 break;
-                case GoldenDel:
-                    System.out.println("Golden Delicious is yellow");
-                    break;
-                    case RedDel:
-                        System.out.println("Red Delicious is red");
-                        break;
-                        case Winesap:
-                            System.out.println("Winesap is red");
-                            break;
-                            case Cortland:
-                                System.out.println("Cortland is red");
-                                break;
-
+            case GoldenDel:
+                System.out.println("Golden Delicious is yellow");
+                break;
+            case RedDel:
+                System.out.println("Red Delicious is red");
+                break;
+            case Winesap:
+                System.out.println("Winesap is red");
+                break;
+            case Cortland:
+                System.out.println("Cortland is red");
+                break;
         }
     }
 
