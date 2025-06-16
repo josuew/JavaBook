8d2252346ff2dd9904c9c55933c81cf9e6b6bbc4
josue_jauregui <josuew2013@gmail.com>
2025-06-15 20:57:07 -0600

    Collections Iterators

---
+++ src/com/softlied/chapter19/CollectionsFramework/example11/MailList.java
@@ -0,0 +1,37 @@
+package com.softlied.chapter19.CollectionsFramework.example11;
+
+import java.util.LinkedList;
+
+class Address{
+    private String name;
+    private String street;
+    private String city;
+    private String state;
+    private String code;
+
+    public Address(String name, String street, String city, String state, String code) {
+        this.name = name;
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.code = code;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "name='" + name + '\'' +
+                ", street='" + street + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", code='" + code + '\'' +
+                '}';
+    }
+}
+
+public class MailList {
+    public static void main(String[] args) {
+        LinkedList<Address> addresses = new LinkedList<Address>();
+
+    }
+}
