9a425bb417466b7d06e8fb1aabad30ffdc539a7a
JaurePerro <josuew2013@gmail.com>
2025-06-21 16:34:15 -0600

    Streams

---
+++ src/com/softlied/chapter29/Streams/example10/StreamDemo10.java
@@ -0,0 +1,12 @@
+package com.softlied.chapter29.Streams.example10;
+
+import java.util.ArrayList;
+
+public class StreamDemo10 {
+    public static void main(String[] args) {
+
+        //Create a list of Strings
+        ArrayList<String> myList = new ArrayList<>();
+
+    }
+}
